b420d505d9ff612ed79b1e07bd6fe965dfb03888
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-04-21 21:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function letterCombinations(input_digit) {
   //Complete the function
+
+
+	
+  if (input_digit.length < 2) {
+    return [input_digit];
+  } else {
+      var allAnswers = [];
+      for (var i = 0; i < input_digit.length; i++) {
+        var letter = input_digit[i];
+        var shorterWord = input_digit.substr(0, i) + input_digit.substr(i + 1, input_digit.length - 1);
+        var shortwordArray = allAnagrams(shorterWord);
+        for (var j = 0; j < shortwordArray.length; j++) {
+          allAnswers.push(letter + shortwordArray[j]);
+        }
+      }
+      return allAnswers;
+  }
+
+
+letterCombinations("")
 }
 
 module.exports = letterCombinations;
